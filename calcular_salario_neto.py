f0e9e11b66810a4cdab59063d73a5f9cee80d3fa
st3v3n030707 <safloreso@uamv.edu.ni>
2025-04-04 18:36:09 -0600

    read me

---
+++ calcular_salario_neto.py
@@ -0,0 +1,4 @@
+salario = 8000
+renta = (8000*0.10)
+seguro = (8000*0.05)
+pensiones = (8000)
---
+++ calcularareayperimetro.py
@@ -0,0 +1,10 @@
+base = int(input("ingrese la base del rectangulo: "))
+altura = int(input("Ingrese la altura del rectangulo: "))
+area = base * altura
+print(f"El areas es :{area}")
+perimetro = (base*2) + (altura*2)
+print(f"El perimetro es: {perimetro}")
+
+
+
+
